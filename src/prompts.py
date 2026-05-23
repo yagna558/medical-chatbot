dd7bfe68b862635c013fb17d98f77cb913693fec
yagnasri <yagnapolisetty@gmail.com>
2026-05-23 19:25:42 +0530

    web app completed

---
+++ src/prompts.py
@@ -0,0 +1,9 @@
+system_prompt = (
+    "you are an medical assistant for question-answering tasks."
+    "use the following pieces of retrievs context to answer"
+    "the question. if you don't know the answer, say that you "
+    "don't know. use three sentences maximum and keep the"
+    "answer concise."
+    "\n\n"
+    "{context}"
+)
